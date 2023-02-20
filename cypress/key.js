3186a3870f52bc7803ec1fb8f3af511b8f04639e
RomanLukach <wisefox17@gmail.com>
2023-02-20 12:42:23 +0300

    Added keys.js

--- cypress/key.js
+++ cypress/key.js
@@ -2,4 +2,5 @@
 // client_id & client_secret generated in Jira / XRAY application / API Keys.
 // client_id & client_secret could be generated for each user.
 
-curl -H "Content-Type: application/json" -X POST --data '{ "client_id": "1049FF0BADCA46988CEDCB8A1A3105BF","client_secret": "cd59c9fe3bdcccbf7bb1e919dea076492857caf1f07d81ec256b55321e37ab88" }'  https://xray.cloud.getxray.app/api/v2/authenticate
+// The following CURL is works on MAC and does not works on Windows
+curl -H "Content-Type: application/json" -X POST --data '{"client_id": "1049FF0BADCA46988CEDCB8A1A3105BF", "client_secret":"cd59c9fe3bdcccbf7bb1e919dea076492857caf1f07d81ec256b55321e37ab88"}' https://xray.cloud.getxray.app/api/v2/authenticate    
